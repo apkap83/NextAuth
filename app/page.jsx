0675ff2178b6273fd8044a5498320a3b63fdbc62
apkap83 <33512036+apkap83@users.noreply.github.com>
2024-05-13 17:02:04 +0300

    OK

--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,7 @@
-import React from "react";
-
-const Home = () => {
+export default function Home() {
   return (
     <div>
       <h1>Home</h1>
     </div>
   );
-};
-
-export default Home;
-Home;
+}
